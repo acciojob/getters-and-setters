d192b265246ba168bc2e28927d8837d90900f3fb
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2023-03-01 15:42:04 +0530

    boilerplte

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+//complete this code
+class Person {}
+
+class Student extends Person {}
+
+class Teacher extends Person {}
+
+// Do not change the code below this line
+window.Person = Person;
+window.Student = Student;
+window.Teacher = Teacher;
